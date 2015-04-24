22d384e28ef2deafc17a75515af538bc654f2434
javilonas <admin@lonasdigital.com>
2015-04-24 20:18:56 +0200

    Update Files and Fix

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1410,9 +1410,9 @@ void mmc_set_data_timeout(struct mmc_data *data, const struct mmc_card *card)
 			 * previous value of 300ms is known to be
 			 * insufficient for some cards.
 			 */
-			limit_us = 3000000;
+			limit_us = 1500000; /* default 300000 */
 		else
-			limit_us = 100000;
+			limit_us = 500000; /* default 100000 */
 
 		/*
 		 * SDHC cards always use these fixed values.
