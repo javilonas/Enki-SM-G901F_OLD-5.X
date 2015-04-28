27e4736d5380fc2f74daefd5026d6fa87c99e9d4
javilonas <admin@lonasdigital.com>
2015-04-29 01:08:27 +0200

    Fix Previous commits (Sorrygit add * --ignore-removal)

--- ramdisk/init.post-boot.sh
+++ ramdisk/init.post-boot.sh
@@ -25,27 +25,6 @@ ln -s /system/xbin/busybox /system/bin/busybox
 # KNOX Off
 /res/ext/eliminar_knox.sh
 
-# allow untrusted apps to read from debugfs
-/system/xbin/supolicy --live \
-	"allow untrusted_app debugfs file { open read getattr }" \
-	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
-	"allow untrusted_app persist_file dir { open read getattr }" \
-	"allow debuggerd gpu_device chr_file { open read getattr }" \
-	"allow netd netd capability fsetid" \
-	"allow netd { hostapd dnsmasq } process fork" \
-	"allow { system_app shell } dalvikcache_data_file file write" \
-	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
-	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
-	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }" \
-	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }" \
-	"allow system_server dex2oat_exec file rx_file_perms" \
-	"allow mediaserver mediaserver_tmpfs file execute" \
-	"allow drmserver theme_data_file file r_file_perms" \
-	"allow zygote system_file file write" \
-	"allow atfwd property_socket sock_file write" \
-	"allow untrusted_app sysfs_display file { open read write getattr add_name setattr remove_name }" \
-	"allow debuggerd app_data_file dir search"
-
 # Detectar si existe el directorio en /system/etc y si no la crea. - by Javilonas
 #
 
